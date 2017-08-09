0b4b2c64947d6cebde842c367e287516b429b4aa
MarvinTeng <tenggang0305@163.com>
2017-08-09 17:41:51 +0800

    add a test class.

---
+++ src/com/helloworld/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.helloworld.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str = "this is a test class.";
+		System.out.println("===>" + str);
+	}
+
+}
